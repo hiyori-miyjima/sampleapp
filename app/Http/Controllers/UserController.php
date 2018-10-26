bb64a875f38081e87dae58fef39d68a560d038fe
宮嶋ひより <hiyori-miyajima@with21.jp>
2018-10-26 20:09:48 +0900

    名前のバリデーションが半角アルファベットであれば通るように変更

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,14 +3,33 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserRegistPost;     // FormRequestクラスをインポートする
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-	// 引数でUserRegistPostクラスのインスタンスを渡す
-	public function register(UserRegistPost $request)
+	public function register(Request $request)
 	{
-		// インスタンスに対して値を問い合わせ
+		// [name]のバリデーションルールに[ascii_alpha]を追加
+		$rules = [
+			'name' 	=> ['required', 'max:20', 'ascii_alpha'],
+			'email' => ['required', 'email', 'max:255'], 
+		];
+
+		$inputs = $request->all();
+
+		// バリデーションルールに[ascii_alpha]を追加
+		Validator::extend('ascii_alpha', function($attribute, $value, $parameters) {
+			// 半角のアルファベットならバリデーションOKとする
+			return preg_match('/^[a-zA-Z]+$/', $value);
+		});
+
+		$validator = Validator::make($inputs, $rules);
+
+		if ($validator->fails()){
+			// ここにバリデーションエラーの場合の処理
+		}
+
+		// バリデーション通過後の処理
 		$name = $request->input('name');
 		$age = $request->input('age');
 	}
